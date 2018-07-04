ef2ed93b0517e4bf0c87e220c37669ecfa442bec
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-07-04 15:50:33 +0300

    Olio-ohjelmoinnin kolmannen kurssipäivän ensimmäinen wpf harjoitus.

---
+++ OlioOhjelmointiWpf/OlioOhjelmointiWpf/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlioOhjelmointiWpf
+{
+    public class Car
+    {
+        public string Color { get; set; }
+        public string Matkustajat { get; set; }
+        public bool Running { get; set; }
+        public bool RadioOn { get; set; }
+
+        public int Vauhti { get; set; }
+
+        public void Start()
+        {
+            Running = true;
+            RadioOn = true;
+        }
+
+        public void Stop()
+        {
+            Running = false;
+            Vauhti = 0;
+        }
+    }
+}
---
+++ OlioOhjelmointiWpf/OlioOhjelmointiWpf/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OlioOhjelmointiWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Car luokan alustaminen
+        public Car auto1 = new Car(); //auto1 = muuttuja
+        public Car auto2 = new Car();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ominaisuuden asettaminen:
+            //auto1 instanssille asetetaan Color ominaisuus punaiseksi:
+            auto1.Color = "Punainen";
+            auto2.Color = "Musta";
+        }
+
+        private void btnAuto1_Click(object sender, RoutedEventArgs e)
+        {
+            //määritellään apumuuttuja:
+            string viesti = "Auto 1 on väriltään " + auto1.Color + ", auto on käynnissä: " +
+                auto1.Running;
+
+            MessageBox.Show(viesti);
+        }
+
+        private void btnAuto2_Click(object sender, RoutedEventArgs e)
+        {
+            //määritellään apumuuttuja:
+            string viesti = "Auto 2 on väriltään " + auto2.Color + ", auto on käynnissä: " +
+                auto2.Running;
+
+            MessageBox.Show(viesti);
+        }
+
+        private void btnStartAuto1_Click(object sender, RoutedEventArgs e)
+        {
+            //Metodin kutsuminen:
+            //auto1.Running = true;
+            auto1.Start();
+        }
+
+        private void btnStartAuto2_Click(object sender, RoutedEventArgs e)
+        {
+            //Metodin kutsuminen:
+            auto2.Start();
+        }
+
+        private void btnStopAuto1_Click(object sender, RoutedEventArgs e)
+        {
+            //Metodin kutsuminen:
+            auto1.Stop();
+        }
+
+        private void btnStopAuto2_Click(object sender, RoutedEventArgs e)
+        {
+            //Metodin kutsuminen:
+            auto2.Stop();
+        }
+    }
+}
