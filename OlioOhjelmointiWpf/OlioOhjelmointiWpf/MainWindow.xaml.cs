c7dfd93c19b3d3303fa585a317b4210742d83fde
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-07-04 16:29:42 +0300

    Lisätty matkustajanlisäys toiminnallisuudet.

--- OlioOhjelmointiWpf/OlioOhjelmointiWpf/MainWindow.xaml.cs
+++ OlioOhjelmointiWpf/OlioOhjelmointiWpf/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace OlioOhjelmointiWpf
 {
@@ -20,9 +21,13 @@ namespace OlioOhjelmointiWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int luku = 123;
+
         //Car luokan alustaminen
         public Car auto1 = new Car(); //auto1 = muuttuja
         public Car auto2 = new Car();
+
+        public DispatcherTimer VauhtiTimer = new DispatcherTimer();
 
         public MainWindow()
         {
@@ -31,13 +36,30 @@ namespace OlioOhjelmointiWpf
             //auto1 instanssille asetetaan Color ominaisuus punaiseksi:
             auto1.Color = "Punainen";
             auto2.Color = "Musta";
+
+            VauhtiTimer.Tick += AutoKiihtyy_Tick;
+            VauhtiTimer.Tick += AutoKiihtyy_Tick2;
+            VauhtiTimer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        private void AutoKiihtyy_Tick(object sender, EventArgs e)
+        {
+            auto1.Vauhti = auto1.Vauhti + 1;
+            lblVauhti.Content = auto1.Vauhti;
+        }
+        private void AutoKiihtyy_Tick2(object sender, EventArgs e)
+        {
+            auto2.Vauhti = auto2.Vauhti + 2;
+            lblVauhti2.Content = auto2.Vauhti;
         }
 
         private void btnAuto1_Click(object sender, RoutedEventArgs e)
         {
             //määritellään apumuuttuja:
-            string viesti = "Auto 1 on väriltään " + auto1.Color + ", auto on käynnissä: " +
-                auto1.Running;
+            string viesti = "Auto 1 on väriltään " +
+                auto1.Color + ", auto on käynnissä: " +
+                auto1.Running + " Matkustajaluettelo: " + auto1.Matkustajat +
+                " auton vauhti on tällä hetkellä: " + auto1.Vauhti;
 
             MessageBox.Show(viesti);
         }
@@ -46,7 +68,8 @@ namespace OlioOhjelmointiWpf
         {
             //määritellään apumuuttuja:
             string viesti = "Auto 2 on väriltään " + auto2.Color + ", auto on käynnissä: " +
-                auto2.Running;
+                auto2.Running + " Matkustajaluettelo: " + auto2.Matkustajat +
+                " auton vauhti on tällä hetkellä: " + auto2.Vauhti; ;
 
             MessageBox.Show(viesti);
         }
@@ -68,12 +91,62 @@ namespace OlioOhjelmointiWpf
         {
             //Metodin kutsuminen:
             auto1.Stop();
+            VauhtiTimer.Stop();
+            lblVauhti.Content = auto1.Vauhti;
         }
 
         private void btnStopAuto2_Click(object sender, RoutedEventArgs e)
         {
             //Metodin kutsuminen:
             auto2.Stop();
+            VauhtiTimer.Stop();
+            lblVauhti2.Content = auto2.Vauhti;
+        }
+
+        private void btnAjaAuto1_Click(object sender, RoutedEventArgs e)
+        {
+            if (auto1.Running)
+            {
+                VauhtiTimer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Käynnistä auto ensin!");
+            }
+        }
+
+        private void btnAjaAuto2_Click(object sender, RoutedEventArgs e)
+        {
+            if (auto2.Running)
+            {
+                VauhtiTimer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Käynnistä auto ensin!");
+            }
+        }
+
+        private void btnLisaaMatkustaja_Click(object sender, RoutedEventArgs e)
+        {
+            auto1.Matkustajat = txtMatkustajat.Text;
+        }
+
+        private void btnLisaaMatkustaja2_Click(object sender, RoutedEventArgs e)
+        {
+            auto2.Matkustajat = txtMatkustajat2.Text;
+        }
+
+        private void btnPoistaMatkustaja_Click(object sender, RoutedEventArgs e)
+        {
+            auto1.Matkustajat = "";
+            txtMatkustajat.Text = "";
+        }
+
+        private void btnPoistaMatkustaja2_Click(object sender, RoutedEventArgs e)
+        {
+            auto2.Matkustajat = "";
+            txtMatkustajat2.Text = "";
         }
     }
 }
